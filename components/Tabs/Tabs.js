ff7fab3424e355d98b137b74b6feb67fb7ac3f0e
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-09 13:33:42 -0500

    Completed questions. MVP completed

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,6 +21,7 @@ class TabLink {
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
+     // .map leaves original array unchanged
      this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
@@ -49,6 +50,7 @@ class TabLink {
      this.tabElement.classList.add('active-tab');
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
+    
      this.cards.forEach(card => card.selectCard());
   }
 }
